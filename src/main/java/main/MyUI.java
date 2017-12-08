65edcde218c43ab8af208eec04b764976cd36126
jmueller95 <julian.mueller@student.uni-tuebingen.de>
2017-12-08 10:48:15 +0100

    Adjust .xml files so the project might show on server

--- src/main/java/main/MyUI.java
+++ src/main/java/main/MyUI.java
@@ -35,8 +35,8 @@ public class MyUI extends UI {
         setContent(layout);
     }
 
-    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    /*@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
-    }
+    }*/
 }
